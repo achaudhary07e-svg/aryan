c9e2c28dfa0f99a7e45326dc91f6dea5b61f5fed
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-24 21:01:18 +0530

    Initial commit

---
+++ LOOP/loop21.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class loop21 {
+      public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+        for (int i=1 ;i<=5;i++){
+            for (int j = 1;j<=5-i;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=(2*i)-1;j++){
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+         for (int i=5 ;i>=1;i--){
+            for (int j = 1;j<=5-i;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=(2*i)-1;j++){
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
