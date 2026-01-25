a63576681694a1c3f3e08081a5472564643858bd
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-25 22:23:00 +0530

    Initial commit

---
+++ Function/function4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class function4 {
+    public static void table(int n){
+        int table=1 ;
+        for(int i =1; i<=10;i++){
+            table= i*n;
+            System.out.println(n+" X "+i+" = "+table);
+        }
+    }
+    public static void main(String args []){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number to find the table of the number");
+        int n = sc.nextInt();
+        table(n);
+    }
+}
