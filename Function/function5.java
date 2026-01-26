6ff680d56e83c0b83beb4514ac15e7d9a296793e
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-26 12:38:45 +0530

    Intial commit

---
+++ Function/function5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class function5{
+    public static void average(int a, int b, int c){
+        float avg= (a+b+c)/3;
+        System.out.println("The average of the number is "+ avg);
+    }
+    public static void main (String arga[]){
+        Scanner sc = new Scanner(System.in );
+        System.out.print("Enter the first number ");
+        int a= sc.nextInt();
+        System.out.print("Enter the first number ");
+        int b= sc.nextInt();
+        System.out.print("Enter the third number ");
+        int c= sc.nextInt();
+        average(a, b, c);
+    }
+}
