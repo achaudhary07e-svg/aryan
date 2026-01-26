127c4891f88b02913f122c2bc954ead88d64c01a
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-26 13:03:19 +0530

    Initial commit

---
+++ Function/function8.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class function8 {
+    public static double circumference(int r ){
+        double c= 2 * 3.14 * r;
+        System.out.println("The Circumferece of the circle is "+ c);
+        return c;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in );
+        System.out.println("Enter the radius of the circle ");
+        int r = sc.nextInt();
+        circumference(r);
+    }
+}
