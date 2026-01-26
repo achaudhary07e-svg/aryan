bec860540ca832e72e2d553e490f8457536d49a7
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-26 13:10:59 +0530

    Initial commit

---
+++ Function/function9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class function9 {
+    public static void ageEligibility(int age ){
+        if (age >=18){
+            System.out.println("You are eligibe to vote");
+        } else {
+            System.out.println ("You are not eligible to vote");
+        }
+        }
+        public static void main(String[] args) {
+            Scanner sc= new Scanner (System. in );
+            System.out.println("Enter your age ");
+            int age = sc.nextInt();
+            ageEligibility(age);
+        }
+}
