47b2e4cdfaeafcc75a38bd7c558b3f218c341145
Aryan chaudhary <achaudhary07e@gmail.com>
2026-01-31 21:53:06 +0530

    Initial Commit

---
+++ Array/array16.java
@@ -0,0 +1,55 @@
+//Write a program to find the sum of all elements in a 2D array.
+import java.util.*;
+public class array16{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.print("Enter the size of rows ");
+        int row=sc.nextInt();
+        System.out.print("Enter the size of columns");
+        int col=sc.nextInt();
+        int arr[][]=new int[col][row];
+        System.out.println("Enter the elements of the array ");
+        for(int i = 0;i<col;i++){
+            for (int j=0;j<row;j++){
+            System.out.print("a "+i+j+" = ");
+            arr[i][j]=sc.nextInt();
+            }
+        }
+        for (int i =0;i<col;i++){
+            for (int j = 0;j<row;j++){
+                System.out.print(arr[i][j]+"  ");
+            }
+            System.out.println();
+        }
+        //Second Array
+         int brr[][]=new int[col][row];
+        System.out.println("Enter the elements of the array ");
+        for(int i = 0;i<col;i++){
+            for (int j=0;j<row;j++){
+            System.out.print("a "+i+j+" = ");
+            brr[i][j]=sc.nextInt();
+            }
+        }
+        for (int i =0;i<col;i++){
+            for (int j = 0;j<row;j++){
+                System.out.print(brr[i][j]+"  ");
+            }
+            System.out.println();
+        }
+        //Sum 
+        int sum[][]=new int[col][row];
+        System.out.println("Enter the elements of the array ");
+        for(int i = 0;i<col;i++){
+            for (int j=0;j<row;j++){
+           sum[i][j]=arr[i][j]+brr[i][j];
+            }
+        }
+        for (int i =0;i<col;i++){
+            for (int j = 0;j<row;j++){
+                System.out.print(sum[i][j]+"  ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
