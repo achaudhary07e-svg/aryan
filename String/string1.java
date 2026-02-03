8649d0539d380984c3ebfa13b60472904227af58
Aryan chaudhary <achaudhary07e@gmail.com>
2026-02-03 20:42:51 +0530

    Initial Commit

---
+++ String/string1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class string1 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in );
+    String name ;
+    System.out.print("Enter the Name of the student ");
+    name=sc.nextLine();
+    System.out.println("The total length of the string is "+ name.length());
+    sc.close();
+}    
+}
